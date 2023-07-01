076e7160170fca7aae89728d80949c668d86864c
Caglar <asdwas94@hotmail.com>
2023-07-01 17:05:52 +0300

    Added New tests

--- Assets/Tests/TimeTickTests/test_time_tick_controller.cs
+++ Assets/Tests/TimeTickTests/test_time_tick_controller.cs
@@ -1,5 +1,8 @@
+using System;
+using NSubstitute;
 using NUnit.Framework;
 using TimeTick;
+using UnityEngine;
 using UnityEngine.TestTools.Utils;
 
 namespace Tests.TimeTickTests
@@ -70,6 +73,100 @@ namespace Tests.TimeTickTests
                     Assert.That(controller.TickTimer, Is.EqualTo(expectedResult).Using(comparer));
                 }
             }
+        }
+
+        [Test]
+        public void Should_Timer_Less_Than_Duration_On_Init()
+        {
+            var controller = new TimeTickController(1.5f, 1);
+
+            Assert.Less(controller.TickTimer, controller.TickDuration);
+        }
+
+        [Test]
+        public void Should_Timer_Greater_or_Equal_To_0()
+        {
+            var controller = new TimeTickController(-1f, 1);
+
+            Assert.GreaterOrEqual(0, controller.TickTimer);
+        }
+
+        [Test]
+        public void Should_Throw_Error_Initiating_Controller_With_0_Duration()
+        {
+            void CreateNewController()
+            {
+                var controller = new TimeTickController(0, 0);
+            }
+
+            Assert.Throws<Exception>(CreateNewController, "Should have throw error.");
+        }
+
+        [Test]
+        public void Should_Throw_Error_Initiating_Controller_Less_Than_0_Duration()
+        {
+            void CreateNewController()
+            {
+                var controller = new TimeTickController(0, -1f);
+            }
+
+            Assert.Throws<Exception>(CreateNewController, "Should have throw error.");
+        }
+
+        [Test]
+        public void Should_Throw_Error_Updating_Controller_With_0_Duration()
+        {
+            var controller = new TimeTickController(0, 1);
+
+            void UpdateControllerDuration()
+            {
+                controller.UpdateTickDuration(0);
+            }
+
+            Assert.Throws<Exception>(UpdateControllerDuration, "Should have throw error.");
+        }
+
+        [Test]
+        public void Should_Throw_Error_Updating_Controller_Less_Than_0_Duration()
+        {
+            var controller = new TimeTickController(0, 1f);
+
+            void UpdateControllerDuration()
+            {
+                controller.UpdateTickDuration(-1f);
+            }
+
+            Assert.Throws<Exception>(UpdateControllerDuration, "Should have throw error.");
+        }
+
+        [Test]
+        public void Should_Update_Tick_Duration()
+        {
+            var controller = new TimeTickController(0, 1);
+            float newDuration = 0.5f;
+            controller.UpdateTickDuration(newDuration);
+
+            Assert.AreEqual(newDuration, controller.TickDuration);
+        }
+
+        [Test]
+        public void Should_Time_Increase_Always_Greater_Than_0()
+        {
+            var controller = new TimeTickController(0, 1);
+            float timeToSubtract = -0.5f;
+            controller.UpdateTimer(timeToSubtract);
+
+            Assert.LessOrEqual(0, controller.TickTimer);
+        }
+
+        [Test]
+        public void Should_Reset_After_Updated_Tick_Duration_If_Exceeded()
+        {
+            var controller = new TimeTickController(0.6f, 1);
+            float newDuration = 0.5f;
+            controller.UpdateTickDuration(newDuration);
+
+            Assert.AreEqual(0, controller.TickTimer);
         }
     }
 }
