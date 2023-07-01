c533354f74db08ced157ded76c03dfbeb245a7c8
Caglar <asdwas94@hotmail.com>
2023-07-01 19:28:18 +0300

    Added Float extension to check if a number exceeded given parameter

---
+++ Assets/Scripts/Extensions/FloatExtensions.cs
@@ -0,0 +1,14 @@
+using Helpers;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class FloatExtensions
+    {
+        public static bool IsExceeded(this float thisFloat, float floatToCompare)
+        {
+            if (thisFloat > floatToCompare) return true;
+            return Mathf.Abs(floatToCompare - thisFloat) < Constants.FloatingPointTolerance;
+        }
+    }
+}
