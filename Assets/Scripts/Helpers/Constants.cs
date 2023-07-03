cc3079cd6573a6ccb03e90dfd27a8404a2f310a2
Caglar <asdwas94@hotmail.com>
2023-07-03 23:34:21 +0300

    Fixed Merge issues

--- Assets/Scripts/Helpers/Constants.cs
+++ Assets/Scripts/Helpers/Constants.cs
@@ -2,11 +2,8 @@ namespace Helpers
 {
     public struct Constants
     {
-<<<<<<< HEAD
         public const double MoneyStartValue = 10;
         public const double GemStartValue = 0;
-=======
         public const float FloatingPointTolerance = 0.0001f;
->>>>>>> systems/tick-system
     }
 }
