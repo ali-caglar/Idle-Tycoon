28b770412b804dd3a0339febde3832bfd176cbe9
Caglar <asdwas94@hotmail.com>
2023-07-09 21:04:42 +0300

    Created Generator manager class

---
+++ Assets/Scripts/Generators/GeneratorManager.cs
@@ -0,0 +1,10 @@
+using Enums;
+using UnityEngine;
+
+namespace Generators
+{
+    public class GeneratorManager : MonoBehaviour
+    {
+        public MultiBuyOption BuyOption { get; private set; }
+    }
+}
